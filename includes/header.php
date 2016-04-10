fdd50479c6c6390211eb5acbee4d429f7abcd504
Matthew Randolph <mrandolph@css.edu>
2016-04-10 08:41:58 -0500

    Character Select Transform and nav fixed

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,7 @@
 			<li><a href="high_score.php">Leaderboard</a></li>
 			<li><a href="view_profile.php">View Profile</a></li>
 			<li><a href="password.php">Change Password</a></li>
-			<li><a href="../character_select.html">Register</a></li>
+			<li><a href="character_select.html">Register</a></li>
 			<li><?php // Create a login/logout link:
 if ( (isset($_COOKIE['user_id'])) && (basename($_SERVER['PHP_SELF']) != 'logout.php') ) {
 	echo '<a href="logout.php">Logout</a>';
